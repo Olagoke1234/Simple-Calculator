419c55a5ed15b3791ac1b06ae5520fce70a83d9b
Olagoke Olorunkosebi <olagokeolorunkosebi@gmail.com>
2022-01-18 15:50:09 +0100

    Done without commas working

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ class Calculator {
   clear() {
     this.currentOperand = " ";
     this.previousOperand = " ";
-    this.operation = " ";
+    this.operation = undefined;
   }
 
   delete() {
@@ -52,14 +52,35 @@ class Calculator {
         return;
     }
     this.currentOperand = computation;
-    this.operation = "";
+    this.operation = undefined;
     this.previousOperand = " ";
   }
+
+  // getDisplayNumber(number) {
+  //   const stringNumber = number.toString();
+  //   const integerDigits = parseFloat(stringNumber.split(".")[0]);
+  //   const decimalDigits = stringNumber.split(".")[1];
+  //   let integerDisplay;
+  //   if (isNaN(integerDigits)) {
+  //     integerDisplay = " ";
+  //   } else {
+  //     integerDisplay = integerDigits.toLocaleString("en", {
+  //       maximumFractionDigits: 0,
+  //     });
+  //   }
+  //   if (decimalDigits !== 0) {
+  //     return `${integerDisplay}.${decimalDigits}`;
+  //   } else {
+  //     return integerDisplay;
+  //   }
+  // }
 
   updateDisplay() {
     this.currentOperandTextElement.innerText = this.currentOperand;
     if (this.operation != null) {
       this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = " ";
     }
   }
 }
